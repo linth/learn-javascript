08f71230bda37821534e92564d7b1128bb91c43e
linth <zhua0404@gmail.com>
2019-11-19 22:54:25 +0800

    add several example of for-loop and let/var variable.

--- js-tutorial/js-basic/for.js
+++ js-tutorial/js-basic/for.js
@@ -2,23 +2,49 @@
 /*
   References:
   - https://www.geeksforgeeks.org/loops-in-javascript/
+  - https://es6.ruanyifeng.com/#docs/let
 */
 
-let i;
+function show_hello_world() {
+  // show hello world several time.
+  let i;
 
-for (i=0; i<10; i++) {
-  console.log('Hello world.');
+  for (i=0; i<10; i++) {
+    console.log('Hello world.');
+  }
 }
+
 
 // method 1.
-let lang = {
-  first : "C",
-  second : "Java",
-  third : "Python",
-  fourth : "PHP",
-  fifth : "JavaScript"
+function show_lang() {
+  let lang = {
+    first : "C",
+    second : "Java",
+    third : "Python",
+    fourth : "PHP",
+    fifth : "JavaScript"
+  }
+
+  // let variable is suitable for for-loop.
+  for (let i in lang) {
+    console.log(i);
+  }
 }
 
-for (let i in lang) {
-  console.log(i);
+function set_an_array() {
+  var a = []
+
+  // please note the variable.
+  // use var variable to
+  for (var i=0; i<10; i++) {
+    a[i] = function () { console.log(i); };
+  }
+  a[6](); // 10
+
+  for (let i=0; i<10; i++) {
+    a[i] =  function () { console.log(i); };
+  }
+  a[6](); // 6
 }
+
+set_an_array()
