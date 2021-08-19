b2ba93a6c090093cad769070011214a41673cfe4
linth <zhua0404@gmail.com>
2021-08-19 15:45:05 +0800

    add url of resource.

--- js-tutorial/js-basic/function/callback/callback-basic.js
+++ js-tutorial/js-basic/function/callback/callback-basic.js
@@ -8,4 +8,5 @@
   References:
     - https://www.sitepoint.com/exceptional-exception-handling-in-javascript/
     - https://pjchender.blogspot.com/2017/12/js-error-handling.html
+    - https://www.liaoxuefeng.com/wiki/1022910821149312/1023024413276544
 */
