2c0a0eb9e1bceb9a9bc20d786f47b335119f93a1
linth <zhua0404@gmail.com>
2022-07-17 16:52:48 +0800

    add for-loop example.

---
+++ operation_conditions/for-loop/for-base.js
@@ -0,0 +1,21 @@
+/**
+ * for-loop 基本範例
+ *  - 從陣列中索引為 0 的單元開始循環。
+ * 
+ * 
+ * 
+ * Reference:
+ *  - https://developer.mozilla.org/zh-TW/docs/Learn/JavaScript/First_steps/Arrays
+ */
+
+
+ var sequence = [1, 1, 2, 3, 5, 8, 13];
+
+
+ for (var i=0; i<sequence.length; i++) {
+    console.log(sequence[i]);
+ }
+
+
+
+
