60d66cb83773b2319cc04b91b717287c509a4d65
linth <zhua0404@gmail.com>
2022-07-17 10:58:14 +0800

    update the usage of private attr.

--- class/build-private-attr.js
+++ class/build-private-attr.js
@@ -1,7 +1,7 @@
 /**
  * 宣告私有欄位
  *  - 從類別外部使用私有欄位會出現錯誤；私有欄位只能在類別內讀寫。
- * 
+ *  - 使用 # + 變數名稱
  * 
  * Reference:
  *  - https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Classes
@@ -25,3 +25,5 @@ r2 = new Rectangle(5, 10);
 console.log(r1); // Rectangle {}
 console.log(r2); // Rectangle {}
 
+console.log(r1.height); // undefined
+console.log(r1.width); // undefined
