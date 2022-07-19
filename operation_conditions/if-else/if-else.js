317781ed04630ae23e8fdbba0e14c774add27428
linth <zhua0404@gmail.com>
2022-07-19 22:48:58 +0800

    add if-else example.

---
+++ operation_conditions/if-else/if-else.js
@@ -0,0 +1,18 @@
+/**
+ * if-else example.
+ * 
+ * 
+ * Reference:
+ *  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+ */
+
+let a = 10;
+let b = 20;
+
+if (a < b) {
+    console.log('a < b');
+} else if (a == b) {
+    console.log('the same.');
+} else {
+    console.log('a > b');
+}
