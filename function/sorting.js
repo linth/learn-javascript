af93aa2b8d38966a1eaacd36bd0f9a68cd797dd3
linth <zhua0404@gmail.com>
2022-07-21 00:33:36 +0800

    update sorting example.

--- function/sorting.js
+++ function/sorting.js
@@ -13,21 +13,30 @@
 const months = ['March', 'Jan', 'Feb', 'Dec'];
 const value = [213, 16, 2058, 55, 11, 1990, 57, 7];
 
-// example 1: this don't sort at all.
+// example 1: this don't sort at all. (會根據 prefix 來排列)
 var res = value.sort();
-// console.log(res); // [11,  16, 1990, 2058, 213,   55, 57,   7]
+console.log('res', res); // [11, 16, 1990, 2058, 213, 55, 57, 7]
+
 
 // example 2: change function to sort the array.
 // TODO: check and understand two functions.
 var l2s = value.sort(function (a, b) {
   // console.log('value1', value1, 'value2', value2, '=>', value2 - value1);
+  // 小到大
   return a - b;
 });
 
+console.log('sortByL2S', l2s); // [7, 11, 16, 55, 57, 213, 1990, 2058]
+
+
 var s2l = value.sort(function (a, b) {
   // console.log('value1', value1, 'value2', value2, '=>', value2 - value1);
+  // 大到小
   return b - a;
 });
 
-console.log('sortByL2S', l2s);
-console.log('sortByS2L', s2l);
+
+// TODO: 似乎會受到影響
+// console.log('res', res); // [2058, 1990, 213, 57, 55, 16, 11, 7]
+// console.log('sortByL2S', l2s); // [2058, 1990, 213, 57, 55, 16, 11, 7]
+console.log('sortByS2L', s2l); // [2058, 1990, 213, 57, 55, 16, 11, 7]
