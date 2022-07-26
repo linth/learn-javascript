f664d4bd226fbef33662b0c53577b816b96f4a66
linth <zhua0404@gmail.com>
2022-07-26 22:36:27 +0800

    add map function example for array.

---
+++ data_structure/array/array-map-fun.js
@@ -0,0 +1,17 @@
+/**
+ * Array map function
+ *  - map() 回傳返回一個 array
+ * 
+ * Reference:
+ *  - https://www.runoob.com/jsref/jsref-map.html
+ */
+
+var numbers = [4, 9, 16, 25];
+
+
+function myFunction(numbers) {
+    return numbers.map(Math.sqrt);
+}
+
+var res = myFunction(numbers);
+console.log(res);
