4018f5851623e067d1d77221c09f11395ff1ff10
linth <zhua0404@gmail.com>
2022-08-08 22:16:09 +0800

    add 'use strict' example.

---
+++ data_structure/map/base-use-strict.js
@@ -0,0 +1,13 @@
+/**
+ * use strict 使用
+ * 
+ * Reference:
+ *  - https://www.liaoxuefeng.com/wiki/1022910821149312/1023024181109440
+ */
+
+'use strict';
+
+var m = new Map();
+// g = 'sss'; // 如果不使用 use strict 則不會報錯誤。
+
+console.log('your browser can support Map.');
