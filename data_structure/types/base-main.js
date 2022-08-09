16cff5109933fefb4d11ad8160a26a88401527a2
linth <zhua0404@gmail.com>
2022-08-09 13:37:04 +0800

    add type example.

---
+++ data_structure/types/base-main.js
@@ -0,0 +1,20 @@
+/**
+ * types: primitives
+ *  - string
+ *  - number
+ *  - boolean
+ *  - null
+ *  - undefined
+ *  - symbol
+ *  - bigint
+ * 
+ * Reference:
+ *  - https://github.com/airbnb/javascript
+ */
+
+const boo = 1;
+let bar = foo;
+
+bar = 9;
+
+console.log(boo, bar); // 
