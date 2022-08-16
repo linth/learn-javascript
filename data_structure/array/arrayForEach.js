9622f34d8efe0a63f2a1db7a30e9bf71559af4f2
linth <zhua0404@gmail.com>
2022-08-16 17:04:05 +0800

    add forEach function for array.

---
+++ data_structure/array/arrayForEach.js
@@ -0,0 +1,14 @@
+/**
+ * ForEach()
+ *  - 
+ * 
+ * Reference:
+ *  - https://www.w3schools.com/jsref/jsref_foreach.asp 
+ */
+
+const fruits = ["apple", "orange", "cherry"];
+
+fruits.forEach(x => {
+    console.log(x);
+});
+
