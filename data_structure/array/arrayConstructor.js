726bde244c18dee406be214081e157b31add8eb1
linth <zhua0404@gmail.com>
2022-08-17 16:21:11 +0800

    add array's constrcutor example.

---
+++ data_structure/array/arrayConstructor.js
@@ -0,0 +1,10 @@
+/**
+ * constructor for array.
+ * 
+ * Reference:
+ *  - https://www.w3schools.com/jsref/jsref_constructor_array.asp
+ */
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits.constructor); // f Array()
+
