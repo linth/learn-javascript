3fddcc71c5749a0d8908cb8b0d05be07a03f9a2a
linth <zhua0404@gmail.com>
2022-08-17 16:59:31 +0800

    add fill() for array example.

---
+++ data_structure/array/arrayFill.js
@@ -0,0 +1,12 @@
+/**
+ * fill for array.
+ *  - Fill all the elements with a value.
+ * 
+ * Reference:
+ *  - https://www.w3schools.com/jsref/jsref_fill.asp
+ */
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const res = fruits.fill("Kiwi");
+
+console.log(res); // ['Kiwi', 'Kiwi', 'Kiwi', 'Kiwi']
