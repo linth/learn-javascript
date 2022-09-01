8ddbee85d16d872188f2e6a48321c99b26f709b3
linth <zhua0404@gmail.com>
2022-09-01 18:04:53 +0800

    add constructor example.

--- function/constructor_method/constructor-base.js
+++ function/constructor_method/constructor-base.js
@@ -6,3 +6,23 @@
  *  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
  */
 
+class A {
+
+}
+
+class B {
+    // TODO: 發現過類似寫法，只有 arguments。沒有 this.xxx = xxx 設定，需要確認。
+    constructor(a) {}
+}
+
+
+class C {
+    constructor(h, w) {
+        this.height = h;
+        this.weight = w;
+    }
+}
+
+
+let c = new C(180, 77);
+console.log(c.height, c.weight); // 180 77
