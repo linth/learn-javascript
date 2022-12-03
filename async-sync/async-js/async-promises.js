e0a8c4a8fa9caf9bfca0b5032ff7189dc0fd105a
linth <zhua0404@gmail.com>
2022-12-03 19:38:06 +0800

    isolate code.

--- async-sync/async-js/async-promises.js
+++ async-sync/async-js/async-promises.js
@@ -5,33 +5,36 @@
  *  - https://www.freecodecamp.org/news/synchronous-vs-asynchronous-in-javascript/
  */
 
-function f1() {
-    console.log('f1');
+{
+    function f1() {
+        console.log('f1');
+    }
+    
+    function f2() {
+        console.log('f2');
+    }
+    
+    function main() {
+        console.log('main');
+        
+        setTimeout(f1, 0);
+        
+        new Promise((resolve, reject) =>
+            resolve('I am a promise')
+        ).then(resolve => console.log(resolve))
+        
+        f2();
+    }
+    
+    main();
+    
+    /**
+     * Results:
+     * 
+     * main
+     * f2
+     * I am a promise
+     * f1
+     */
 }
 
-function f2() {
-    console.log('f2');
-}
-
-function main() {
-    console.log('main');
-    
-    setTimeout(f1, 0);
-    
-    new Promise((resolve, reject) =>
-        resolve('I am a promise')
-    ).then(resolve => console.log(resolve))
-    
-    f2();
-}
-
-main();
-
-/**
- * Results:
- * 
- * main
- * f2
- * I am a promise
- * f1
- */
