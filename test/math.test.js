c9066ab6f06c2c7d16b7ebd876c79190f04b6e88
linth <zhua0404@gmail.com>
2022-12-03 20:18:19 +0800

    update test example.

--- test/math.test.js
+++ test/math.test.js
@@ -4,7 +4,14 @@ const { add, mul, sub, div } = require('../test/math')
 
 test('test add', () => {
     expect(add(2, 3)).toBe(5);
+    expect(add(3, 3)).toBe(6);
 });
+
+test('test not add', () => {
+    expect(add(1, 2)).not.toBe(4);
+    expect(add(1, 2)).not.toBe(5);
+    expect(add(-1, 7)).not.toBe(5);
+})
 
 test('test mul', () => {
     expect(mul(3, 4)).toBe(12);
