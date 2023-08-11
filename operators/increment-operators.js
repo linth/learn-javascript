34b2f03b25b35920177638fb9cb46c99fc4c8fa1
linth <zhua0404@gmail.com>
2023-08-12 00:18:03 +0800

    add increment/decrement operators.

---
+++ operators/increment-operators.js
@@ -0,0 +1,14 @@
+/**
+ * increment/decrement 遞增運算子
+ * 
+ * Reference:
+ *  - https://www.cythilya.tw/2018/10/09/intro-1/
+ */
+
+{
+  // ++, --
+  var a = 4, b = 10;
+  a++;
+  console.log(a); // 5
+  console.log(b++); // 10
+}
