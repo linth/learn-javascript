b30cf0e2c3d91058887cace804c1711d9db5b7c7
linth <zhua0404@gmail.com>
2023-08-11 23:58:41 +0800

    add type js comment.

---
+++ data-type/type-js-basic-8.js
@@ -0,0 +1,17 @@
+/**
+ * JS Built-in types
+ *  - number
+ *  - string
+ *  - boolean
+ *  - null
+ *  - undefined
+ *  - object
+ *  - symbol 
+ * 
+ * 內建型別又分兩大類
+ *  - 基本型別 (primitives)
+ *  - 物件型別 (object)
+ */
+{
+  
+}
