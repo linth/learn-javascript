b9cb0c77a3e5cf37ff8cc475756420a4e75d0211
linth <zhua0404@gmail.com>
2023-08-12 00:18:19 +0800

    add compound assignment operators.

---
+++ operators/compound-assignment-operators.js
@@ -0,0 +1,14 @@
+/**
+ * compound assignment operators (複合指定運算子)
+ * 
+ * Reference:
+ *  - https://www.cythilya.tw/2018/10/09/intro-1/
+ */
+
+{
+  // +=, -=, *=, /=
+
+  var a = 1;
+  a += 1;  // a = a + 1
+  console.log(a); // 2
+}
