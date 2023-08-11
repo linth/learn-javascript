4d4ff64502db76543514a2dc7bc979af2cb119c1
linth <zhua0404@gmail.com>
2023-08-12 00:18:32 +0800

    add math operators.

---
+++ operators/math-operators.js
@@ -0,0 +1,13 @@
+/**
+ * math operators (算數運算子)
+ * 
+ * Reference:
+ *  - https://www.cythilya.tw/2018/10/09/intro-1/
+ */
+
+{
+  // +, -, *, /
+  var i = 1;
+  console.log(i+1); // 2
+  console.log(i*100); // 100
+}
