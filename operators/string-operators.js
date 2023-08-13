e6e5a5af4e3a0d6445467606162a2307f308ff96
linth <zhua0404@gmail.com>
2023-08-14 01:38:08 +0800

    add string operators

---
+++ operators/string-operators.js
@@ -0,0 +1,11 @@
+/**
+ * string operators (字串運算子)
+ * 
+ * 
+ */
+{
+  const name = 'george ';
+  const greeting_string = 'hello world.';
+
+  console.log(name + greeting_string); // george hello world.
+}
