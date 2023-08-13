0acd9ddd14b5eff1d50892ef9c6311ad60c79c2e
linth <zhua0404@gmail.com>
2023-08-14 01:37:32 +0800

    add comparison operators

---
+++ operators/comparison-operators.js
@@ -0,0 +1,24 @@
+/**
+ * comparison operators (比較運算子)
+ * 
+ * 
+ */
+
+{
+  // < (小於), > (大於), <= (小於等於), >= (大於等於)
+  var a = 4;
+  var b = 10;
+  var c = 10;
+
+  if (a>b) {
+    console.log('a大於b');
+  } else {
+    console.log('a小於b'); // a小於b
+  }
+
+  if (c >= b) {
+    console.log('c大於等於b'); // c大於等於b
+  } else {
+    console.log('c小於b');
+  }
+}
