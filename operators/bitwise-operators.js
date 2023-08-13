d00f226c522d18d3d0fa5658859d983db3f14ed3
linth <zhua0404@gmail.com>
2023-08-14 01:37:22 +0800

    add bitwise operators

---
+++ operators/bitwise-operators.js
@@ -0,0 +1,14 @@
+/**
+ * bitwise operators (位元運算子)
+ *  - 將運算元當成 32 位元的0和1來看
+ * 
+ * Reference:
+ *  - https://www.w3schools.com/js/js_bitwise.asp
+ *  - https://www.cythilya.tw/2018/10/09/intro-1/
+ */
+{
+  // 5: 0101, 1: 0001
+  console.log(5 & 1); // 1
+  console.log(5 | 1); // 5
+  console.log(~5); // -6
+} 
