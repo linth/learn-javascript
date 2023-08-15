62078671e270608d17ab4c18dbb2af34a66ef4b6
linth <zhua0404@gmail.com>
2023-08-16 00:40:14 +0800

    update for-in example.

--- operation_conditions/for-loop/for-in.js
+++ operation_conditions/for-loop/for-in.js
@@ -8,31 +8,33 @@ Note:
     - 要注意的是在列舉物件屬性時很重要的是：使用hasOwnProperty()來過濾掉原型（prototype）屬性。
 */
 
-var i,
+{
+    var i,
     man = {
         hands: 2,
         legs: 2,
         heads: 1,
     };
 
-if (typeof Object.prototype.clone == 'underfined') {
-    Object.prototype.clone = function() {
+    if (typeof Object.prototype.clone == 'underfined') {
+        Object.prototype.clone = function() {
 
-    };
-}
-
-
-// 有用hasOwnProperty來過濾
-for (i in man) {
-    if (man.hasOwnProperty(i)) {
-        console.log(i, ":", man[i]);
+        };
     }
-}
 
-console.log('');
 
-// 未使用hasOwnProperty來過濾
-// TODO: something should be re-checked.
-for (i in man) {
-    console.log(i, ':', man[i]);
+    // 有用hasOwnProperty來過濾
+    for (i in man) {
+        if (man.hasOwnProperty(i)) {
+            console.log(i, ":", man[i]);
+        }
+    }
+
+    console.log('');
+
+    // 未使用hasOwnProperty來過濾
+    // TODO: something should be re-checked.
+    for (i in man) {
+        console.log(i, ':', man[i]);
+    }
 }
