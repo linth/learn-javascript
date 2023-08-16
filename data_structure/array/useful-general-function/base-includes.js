493110ee8d4272262515c0c61db7224541a0a4cf
gerogelin <gerogelin@oringnet.com>
2023-08-16 18:27:36 +0800

    fix the result of error.

--- data_structure/array/useful-general-function/base-includes.js
+++ data_structure/array/useful-general-function/base-includes.js
@@ -6,11 +6,13 @@
  *  - https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
  */
 
-const nums = [1, 2, 3, 7, 6, 1];
-const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+{
+  const nums = [1, 2, 3, 7, 6, 1];
+  const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
 
-console.log(nums.includes(6)); // true
-console.log(nums.includes(9)); // false
+  console.log(nums.includes(6)); // true
+  console.log(nums.includes(9)); // false
 
-console.log(beasts.includes('ant')); // true
-console.log(beasts.includes('ducks')); // true
+  console.log(beasts.includes('ant')); // true
+  console.log(beasts.includes('ducks')); // false
+}
