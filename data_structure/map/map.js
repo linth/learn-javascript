ee07b7a1391b37fc2e51592be11194265bb6d29f
gerogelin <gerogelin@oringnet.com>
2023-08-18 18:41:25 +0800

    update map example.

--- data_structure/map/map.js
+++ data_structure/map/map.js
@@ -21,57 +21,59 @@
     - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
 */
 
-let m = new Map();
+{
+  let m = new Map();
 
-m.set('name', 'george');
-m.set('age', 30);
-m.set(1, 'hello world.');
+  m.set('name', 'george');
+  m.set('age', 30);
+  m.set(1, 'hello world.');
 
-console.log(m); // { 'name' => 'george', 'age' => 30, 1 => 'hello world.' }
-console.log(m.size); // 3
-console.log(m.get('name')); // george
-console.log(m.get(1)); // hello world.
-
-
-// iteration over Map.
-// map.keys(), map.values(), map.entries()
-let student = new Map([
-    ['george', 39],
-    ['Peter', 55],
-    ['May', 33],
-    ['HaHa', 20],
-])
+  console.log(m); // { 'name' => 'george', 'age' => 30, 1 => 'hello world.' }
+  console.log(m.size); // 3
+  console.log(m.get('name')); // george
+  console.log(m.get(1)); // hello world.
 
 
-// iterate over keys.
-for (let s of student.keys()) {
-    console.log('keys: ', s); // keys: george Peter May HaHa
+  // iteration over Map.
+  // map.keys(), map.values(), map.entries()
+  let student = new Map([
+      ['george', 39],
+      ['Peter', 55],
+      ['May', 33],
+      ['HaHa', 20],
+  ])
+
+
+  // iterate over keys.
+  for (let s of student.keys()) {
+      console.log('keys: ', s); // keys: george Peter May HaHa
+  }
+
+  // iterate over values.
+  for (let s of student.values()) {
+      console.log('values: ', s); // values: 39 55 33 20
+  }
+
+  // iterate over [key, value] entries.
+  for (let s of student) {
+      console.log('entries: ', s); // entries:  [ 'george', 39 ] ...
+  }
+
+
+  // Map from Object.
+  let obj = {
+      name: 'John',
+      age: 30,
+  }
+
+  console.log(obj);
+
+  let map = new Map(Object.entries(obj));
+  console.log(map.get('name'));
+  console.log('-->', Object.entries(obj)); // --> [ [ 'name', 'John' ], [ 'age', 30 ] ]
+
+
+  console.log(student);
+  let student2 = Object.fromEntries(student);
+  console.log(student2, typeof(student2)); // { george: 39, Peter: 55, May: 33, HaHa: 20 } object
 }
-
-// iterate over values.
-for (let s of student.values()) {
-    console.log('values: ', s); // values: 39 55 33 20
-}
-
-// iterate over [key, value] entries.
-for (let s of student) {
-    console.log('entries: ', s); // entries:  [ 'george', 39 ] ...
-}
-
-
-// Map from Object.
-let obj = {
-    name: 'John',
-    age: 30,
-}
-
-console.log(obj);
-
-let map = new Map(Object.entries(obj));
-console.log(map.get('name'));
-console.log('-->', Object.entries(obj)); // --> [ [ 'name', 'John' ], [ 'age', 30 ] ]
-
-
-console.log(student);
-let student2 = Object.fromEntries(student);
-console.log(student2, typeof(student2)); // { george: 39, Peter: 55, May: 33, HaHa: 20 } object
