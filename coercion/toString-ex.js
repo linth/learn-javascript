b52888d2300d0b515a1fe3b973c16a9686833b9e
linth <zhua0404@gmail.com>
2023-08-18 23:06:13 +0800

    add ToString function example.

---
+++ coercion/toString-ex.js
@@ -0,0 +1,20 @@
+/**
+ * ToString
+ * 
+ * 
+ * Reference:
+ *  - https://www.cythilya.tw/2018/10/15/coercion/
+ */
+
+{
+  // undefined -> 'undefined'
+  // null -> 'null'
+  
+  var a = true.toString();
+  console.log(a); // true
+
+  var b = [1, 2, 3].toString();
+  console.log(b); // 1,2,3
+
+
+}
