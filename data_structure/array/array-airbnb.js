62b4a082d22ae488f620ab5788a3f8e347e079b9
linth <zhua0404@gmail.com>
2023-08-27 01:05:49 +0800

    update array for airbnb example.

--- data_structure/array/array-airbnb.js
+++ data_structure/array/array-airbnb.js
@@ -50,6 +50,7 @@
 
   // 4.5 Use Array.from for converting an array-like object to an array.
   // 使用 Array.from 來處理 array-like 物件轉換成 array.
+  // 當如果要處理 object -> array, 使用 Array.from
   const arrLike = { 
     0: 'foo', 
     1: 'bar', 
@@ -65,8 +66,59 @@
   console.log(arrs); // [ 'foo', 'bar', 'baz' ]
 
 
-  // TODO: 4.6 example.
+  // 4.6 Use Array.from instead of spread ... for mapping over iterables, because it avoids creating an intermediate array.
+  // bad
+  const baz = [...foo].map(bar);
 
+  // good
+  const bazs = Array.from(foo, bar);
+  console.log(bazs);
+
+
+  // 4.7 Use return statements in array method callbacks. It’s ok to omit the return if the function body consists of a single statement returning an expression without side effects, following 8.2. eslint: array-callback-return
+  // good
+  [1, 2, 3].map((x) => {
+    const y = x + 1;
+    return x * y;
+  });
+
+  // good
+  [1, 2, 3].map((x) => x + 1);
+
+  // bad - no returned value means `acc` becomes undefined after the first iteration
+  [[0, 1], [2, 3], [4, 5]].reduce((acc, item, index) => {
+    const flatten = acc.concat(item);
+  });
+
+  // good
+  [[0, 1], [2, 3], [4, 5]].reduce((acc, item, index) => {
+    const flatten = acc.concat(item);
+    return flatten;
+  });
+
+  // bad
+  inbox.filter((msg) => {
+    const { subject, author } = msg;
+    if (subject === 'Mockingbird') {
+      return author === 'Harper Lee';
+    } else {
+      return false;
+    }
+  });
+
+  // good
+  inbox.filter((msg) => {
+    const { subject, author } = msg;
+    if (subject === 'Mockingbird') {
+      return author === 'Harper Lee';
+    }
+
+    return false;
+  });
+
+
+
+  // TODO: 4.8 example.
   // 
   // bad
   // good
