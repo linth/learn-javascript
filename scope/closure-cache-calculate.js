e8d6a3ca38935c8f1658b2ff18b7c735a74b9b39
linth <zhua0404@gmail.com>
2023-10-08 01:23:56 +0800

    add js closure to cache the result of calculating.

---
+++ scope/closure-cache-calculate.js
@@ -0,0 +1,38 @@
+/**
+ * closure 閉包
+ *  - 如果希望能記住上次計算的值，不用再算一次，就可以使用閉包。
+ * 
+ * 
+ * Reference:
+ *  - https://v61265.coderbridge.io/2020/10/23/%E7%B5%A6%E8%87%AA%E5%B7%B1%E7%9C%8B%E7%9A%84-js-%E9%80%B2%E9%9A%8E%EF%BC%8Dclosure/
+ */
+
+
+
+{
+  function square(n) {
+    console.log('calculate square.');
+    return n*n;
+  }
+
+  function cache_before_calculating(func) {
+    var ans = {}
+
+    return function(num) {
+      if(ans[num]) {
+        return ans[num]
+      }
+
+      ans[num] = func(num)
+      return ans[num]
+    }
+  }
+
+  const cbc = cache_before_calculating(square);
+  console.log(cbc(20));
+  console.log(cbc(20));
+  console.log(cbc(20));
+
+  // TODO: think about why square() cannot show log information.
+}
+
