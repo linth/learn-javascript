d445231c2b2d2dd94ae5ba4f34154ca6607efa5a
gerogelin <gerogelin@oringnet.com>
2024-01-06 01:54:53 +0800

    feat: add comments.

--- js-tutorial/js-basic/2023-new-task.js
+++ js-tutorial/js-basic/2023-new-task.js
@@ -3,4 +3,8 @@
  * 
  * Reference:
  *  - https://ithelp.ithome.com.tw/users/20162568/ironman/6444
+ *  - https://github.com/airbnb/javascript
+ *  - https://github.com/javascript-tutorial/en.javascript.info
+ *  - https://github.com/trekhleb/javascript-algorithms
+ *  - https://github.com/Asabeneh/30-Days-Of-JavaScript
  */
