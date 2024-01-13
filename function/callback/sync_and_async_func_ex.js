693c030d26c0f9ac1799eb0ffd869aa050af8b1c
linth <zhua0404@gmail.com>
2024-01-14 01:39:54 +0800

    feat: add sync and async function example.

---
+++ function/callback/sync_and_async_func_ex.js
@@ -0,0 +1,57 @@
+/**
+ * sync & async function example.
+ * 
+ * @param {*} cb 
+ */
+
+
+{
+  async function execute_two_cbs(cb) {
+    console.log('start execute_two_cbs().');
+    // 這種是同步執行的回調函式
+    cb();
+    cb1();
+    cb3();
+    console.log('end execute_two_cbs().');
+  }
+  
+  async function cb1() {
+    console.log('cb 1');
+  }
+  
+  async function cb2() {
+    console.log('cb 2');
+  }
+
+  function cb3() {
+    console.log('cb 3');
+  }
+  
+  execute_two_cbs(cb2);
+  /**
+   * start execute_two_cbs().
+   * cb 2
+   * cb 1
+   * cb 3
+   * end execute_two_cbs().
+   */
+}
+
+{
+  function execute_async_cbs(cb) {
+    console.log('start execute_async_cbs().');
+    setTimeout(cb, 0);
+    console.log('end execute_async_cbs().');
+  }
+
+  function cb4() {
+    console.log('cb 4');
+  }
+
+  execute_async_cbs(cb4);
+  /**
+   * start execute_async_cbs().
+   * end execute_async_cbs().
+   * cb 4
+   */
+}
