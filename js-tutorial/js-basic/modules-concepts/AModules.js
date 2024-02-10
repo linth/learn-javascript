eeae809f4a979224e9c829914445003ba5a7ae82
linth <zhua0404@gmail.com>
2024-02-10 15:04:49 +0800

    feat: add class, function modules with require way.

---
+++ js-tutorial/js-basic/modules-concepts/AModules.js
@@ -0,0 +1,17 @@
+// class
+class AModules {
+  showClassName() {
+    console.log(`This is an AModules class.`);
+  }
+}
+
+// function
+let aa = function() {
+  console.log('aa function');
+}
+
+module.exports = {
+  aa,
+  AModules,
+}
+
---
+++ js-tutorial/js-basic/modules-concepts/mainModules.js
@@ -0,0 +1,10 @@
+const { aa, AModules } = require('./AModules')
+
+{
+  // class 
+  const a = new AModules();
+  a.showClassName();
+
+  // function
+  aa();
+}
