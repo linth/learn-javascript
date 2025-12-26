20274fc9d9770deb30cf96531ad0cdbf2c832873
linth <zhua0404@gmail.com>
2025-12-26 14:32:57 +0800

    feat: update function base-example.

--- function/function-basic.js
+++ function/function-basic.js
@@ -1,4 +1,10 @@
 /*
+函式由四部分組成：
+  - 關鍵字 function
+  - 可有可無的名稱，必須合法的 javascript 識別字
+  - 參數、引數，需用（）來包起來內，也必須是合法識別字，不能省略
+  - 需要 {} 來包起來程式述敘句，函式主體可空，但是不能省略 {}
+
 Goal: how to use function concept to isolate another code.
   - control flow
   - imperative & declarative
@@ -10,6 +16,52 @@ keyword:
 References:
   - https://www.geeksforgeeks.org/understanding-variable-scopes-in-javascript/
 */
+const assert = require("assert");
+
+{
+  // 具名函式
+  function helloWold() {
+    console.log('hello world.');
+  }
+
+  // 匿名函式
+  var canEat = function () {
+    return true; // 建立匿名函式，指派給變數 canEat，函式的屬性 name 是空的
+  }
+  console.log(typeof canEat === 'function'); // true
+}
+
+{
+  // inner / outer function
+  function outer() {
+    var a = 1;
+    console.log('outer function.', a, b, c); // outer function. 1 undefined undefined
+
+    function inner() {
+      console.log('inner function.', a, b, c);
+    }
+    var b = 2;
+    if (a == 1) {
+      var c = 3;
+    }
+    console.log(a, b, c);  // 1, 2, 3
+  }
+
+  outer();
+
+  /**
+   * 各宣告的範圍：
+   *  - outer() 從頭到位的範圍
+   *  - inner() 從 outer 函式開始到結束 (outer() { .... })
+   *  - a: 從 var a = 1 開始到 outer 函式結束
+   *  - b: 從 var b = 2 開始到 outer 函式結束
+   *  - c: 從 var c = 3 開始到 outer 函式結束
+   * 
+   * 另外，你可以嘗試將 inner 函式放到不同位置看看！
+   */
+}
+
+
 
 // example 1:
 let global_var = 'This is a global variable.'
