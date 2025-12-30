f7c2ff93307a35dc47b51670214bf1c1842b2abd
linth <zhua0404@gmail.com>
2025-12-30 15:39:35 +0800

    feat: update prototype example.

--- function/prototype/function-class-inherit.js
+++ function/prototype/function-class-inherit.js
@@ -1,28 +1,35 @@
 /**
+ * JS 不是 class 語言，它是 prototype 語言
+ *  - JS 不是：每個物件從 class 產生
+ *  - JS 是：每個物件都是「從另一個物件 clone 出來」，這個「原始物件」叫 prototype。
+ * 
+ * 
  * 當兩個物件無繼承關係
- *  - 類別並無法擴充一般(non-constructible不可建構的)物件。如果您想要繼承自一般的物件，可以使用Object.setPrototypeOf() (en-US)來達成。
+ *  - 類別並無法擴充一般(non-constructible不可建構的)物件。
+ *  - 如果您想要繼承自一般的物件，可以使用Object.setPrototypeOf() (en-US)來達成。
  * 
  * Reference:
  *  - https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Classes#%E7%94%A8_extends_%E5%BB%BA%E7%AB%8B%E5%AD%90%E9%A1%9E%E5%88%A5
  */
 
 
-var Animal = {
+{
+  var Animal = {
     speak() {
-        console.log(this.name + ' make a noise.');
+      console.log(this.name + ' make a noise.');
     }
-}
+  }
 
-
-class Dog {
+  class Dog {
     constructor(name) {
-        this.name = name;
+      this.name = name;
     }
+  }
+
+  // 使用 object 的 setPrototypeOf 
+  Object.setPrototypeOf(Dog.prototype, Animal);
+
+  var d = new Dog('Haha');
+  d.speak(); // Haha make a noise.
 }
-
-// 使用 object 的 setPrototypeOf 
-Object.setPrototypeOf(Dog.prototype, Animal);
-
-var d = new Dog('Haha');
-d.speak(); // Haha make a noise.
 
--- function/prototype/prototype-base.js
+++ function/prototype/prototype-base.js
@@ -1,4 +1,14 @@
 /**
+ * JS 不是 class 語言，它是 prototype 語言
+ *  - JS 不是：每個物件從 class 產生
+ *  - JS 是：每個物件都是「從另一個物件 clone 出來」，這個「原始物件」叫 prototype。
+ * 
+ * 
+ * 共享資料/資源，節省記憶體
+ * 動態熱插功能
+ * runtime 擴充能力
+ * 
+ * 
  * prototype-base method
  * 
  * TODO: 增加多點解釋跟範例。
@@ -7,24 +17,43 @@
  *  - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
  */
 
- class Polygon {
+{
+  // 利用函式存儲資料，方法則用prototype.function_name來clone出來，共享資料/資源。
+  function User(name) {
+    this.name = name;
+  }
+
+  User.prototype.sayHi = function () {
+    return `Hi, I'm ${this.name}`;  // sayHi 只存在一份，不是每個 instance 一份。
+  };
+
+  const u1 = new User('George');
+  const u2 = new User('Amy');
+
+  console.log(u1.sayHi());
+  console.log(u2.sayHi());
+}
+
+
+{
+  class Polygon {
     constructor(h, w) {
-        this.height = h;
-        this.width = w;
+      this.height = h;
+      this.width = w;
     }
 
     // Getter
     get area() {
-        return this.calcArea();
+      return this.calcArea();
     }
 
     // Method
     calcArea() {
-        return this.height * this.width;
+      return this.height * this.width;
     }
+  }
+
+  const square = new Polygon(10, 10);
+  console.log(square.area);
 }
-
-
-const square = new Polygon(10, 10);
-console.log(square.area);
 
