8fd75d8dc800505fdea6bafaba20311a4bab675a
linth <zhua0404@gmail.com>
2025-12-30 21:05:57 +0800

    feat: update class-base example.

--- class/class-base.js
+++ class/class-base.js
@@ -1,23 +1,53 @@
 /**
  * class-base 範例
+ *  - ES6 Class 本質（不是新語言，是 Prototype 的語法糖）
  * 
  * Reference:
  *  - https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Classes
  */
 
-// 寫法 1: named
-class Person {
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
+{
+  class UserService {
+    constructor(name) {
+      this.name = name;
     }
+
+    hello() {
+      return `Hello, ${this.name}`;
+    }
+  }
+
+  // 等價於：
+  function User2Service(name) {
+    this.name = name;
+  }
+
+  User2Service.prototype.hello = function () {
+    return `Hello, ${this.name}`;
+  }
+
+  var res = User2Service.prototype.hello;
+  console.log('res', res); // res [Function (anonymous)]
+  
 }
 
 
-// 寫法 2: unnamed
-var Person = class {
-    constructor(height, width) {
-        this.height = height;
-        this.width = width;
-    }
+
+
+{
+  // // 寫法 1: named
+  // class Person {
+  //   constructor(height, width) {
+  //     this.height = height;
+  //     this.width = width;
+  //   }
+  // }
+
+  // // 寫法 2: unnamed
+  // var Person = class {
+  //   constructor(height, width) {
+  //     this.height = height;
+  //     this.width = width;
+  //   }
+  // }
 }
